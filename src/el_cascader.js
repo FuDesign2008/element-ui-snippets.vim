53c695003022ae24494a9662407b02f2097aeb61
fudesign2008 <fudesign2008@163.com>
2019-01-14 11:06:16 +0800

    new file:   UltiSnips/javascript_el_cascader.snippets         new file:   UltiSnips/vue_el_cascader.snippets         new file:   UltiSnips/vue_el_input_number.snippets         new file:   UltiSnips/vue_el_select.snippets         new file:   UltiSnips/vue_el_slider.snippets         new file:   UltiSnips/vue_el_switch.snippets         new file:   src/el_cascader.js

---
+++ src/el_cascader.js
@@ -0,0 +1,26 @@
+const options = [
+  {
+    value: '1',
+    label: '一级-1',
+    disabled: false,
+    children: [
+      {
+        value: '1-1',
+        label: '二级-1-1',
+        disabled: false,
+      },
+    ],
+  },
+  {
+    value: '2',
+    label: '一级-2',
+    disabled: false,
+    children: [
+      {
+        value: '2-1',
+        label: '二级-2-1',
+        disabled: false,
+      },
+    ],
+  },
+]
