4945e818eaba243406deb4250164eedd96fa2273
fudesign2008 <fudesign2008@163.com>
2021-08-10 15:21:46 +0800

    modified:   UltiSnips/javascriptreact_el_alert.snippets   modified:   UltiSnips/javascriptreact_el_badge.snippets   modified:   UltiSnips/javascriptreact_el_basic.snippets   modified:   UltiSnips/javascriptreact_el_bread_crumb.snippets   modified:   UltiSnips/javascriptreact_el_button.snippets   modified:   UltiSnips/javascriptreact_el_cascader.snippets   modified:   UltiSnips/javascriptreact_el_checkbox.snippets   modified:   UltiSnips/javascriptreact_el_color_picker.snippets   modified:   UltiSnips/javascriptreact_el_datepicker.snippets   modified:   UltiSnips/javascriptreact_el_datetimepicker.snippets   modified:   UltiSnips/javascriptreact_el_dialog.snippets   modified:   UltiSnips/javascriptreact_el_dropdown.snippets   modified:   UltiSnips/javascriptreact_el_form.snippets   modified:   UltiSnips/javascriptreact_el_input.snippets   modified:   UltiSnips/javascriptreact_el_input_number.snippets   modified:   UltiSnips/javascriptreact_el_link.snippets   modified:   UltiSnips/javascriptreact_el_loading.snippets   modified:   UltiSnips/javascriptreact_el_nav_menu.snippets   modified:   UltiSnips/javascriptreact_el_pagination.snippets   modified:   UltiSnips/javascriptreact_el_progress.snippets   modified:   UltiSnips/javascriptreact_el_radio.snippets   modified:   UltiSnips/javascriptreact_el_rate.snippets   modified:   UltiSnips/javascriptreact_el_select.snippets   modified:   UltiSnips/javascriptreact_el_slider.snippets   modified:   UltiSnips/javascriptreact_el_steps.snippets   modified:   UltiSnips/javascriptreact_el_switch.snippets   modified:   UltiSnips/javascriptreact_el_table.snippets   modified:   UltiSnips/javascriptreact_el_tabs.snippets   modified:   UltiSnips/javascriptreact_el_tag.snippets   modified:   UltiSnips/javascriptreact_el_timepicker.snippets   modified:   UltiSnips/javascriptreact_el_transition.snippets   modified:   UltiSnips/javascriptreact_el_upload.snippets   modified:   convertToJsx.ts

--- convertToJsx.ts
+++ convertToJsx.ts
@@ -12,10 +12,13 @@ const dirPath = path.join(__dirname, 'UltiSnips')
 const files = fs.readdirSync(dirPath)
 
 files.forEach(item => {
-  if (item.indexOf('javascript_jsx_el_') === 0) {
+  if (item.indexOf('javascriptreact_el_') === 0) {
     const sourcePath = path.join(__dirname, 'UltiSnips', item)
-    const destFile = item.replace('javascript_jsx_el_', 'javascriptreact_el_')
-    const destPath = path.join(__dirname, 'UltiSnips', destFile)
-    fs.copyFileSync(sourcePath, destPath)
+    const content = fs.readFileSync(sourcePath, 'utf-8')
+    const changed = content.replace('extends vue', '# extends javascriptreact')
+    fs.writeFileSync(sourcePath, changed, 'utf-8')
+    // const destFile = item.replace('javascript_jsx_el_', 'javascriptreact_el_')
+    // const destPath = path.join(__dirname, 'UltiSnips', destFile)
+    // fs.copyFileSync(sourcePath, destPath)
   }
 })
